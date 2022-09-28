eb848e55f8c68ed8b0562f0eb80f95b83d118420
trujillo.agut.carles <trujillo.agut.carles@alumnat.copernic.cat>
2022-09-28 09:23:25 +0200

    Logo, colores y temas añadidos

--- app/src/main/java/cat/copernic/fpshare/MainActivity.kt
+++ app/src/main/java/cat/copernic/fpshare/MainActivity.kt
@@ -1,7 +1,7 @@
 package cat.copernic.fpshare
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
