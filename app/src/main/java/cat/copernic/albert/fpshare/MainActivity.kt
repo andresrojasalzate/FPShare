49ab45f2929126e7fbe0498d952f8b65dc52152a
=Albert Montero <=montero.villar.albert@alumnat.copernic.cat>
2022-10-04 13:20:52 +0200

    Testeo para guia git.

--- app/src/main/java/cat/copernic/albert/fpshare/MainActivity.kt
+++ app/src/main/java/cat/copernic/albert/fpshare/MainActivity.kt
@@ -8,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Hola, esto es una prueba en mi proyecto.
     }
 }
