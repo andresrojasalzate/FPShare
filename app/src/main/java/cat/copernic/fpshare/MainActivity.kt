078491da323af193ea18efc8beac43ac4627dca9
=Albert Montero <=montero.villar.albert@alumnat.copernic.cat>
2022-10-04 10:55:52 +0200

    Inicio del Proyecto

--- app/src/main/java/cat/copernic/fpshare/MainActivity.kt
+++ app/src/main/java/cat/copernic/fpshare/MainActivity.kt
@@ -2,7 +2,6 @@ package cat.copernic.fpshare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import cat.copernic.albert.fpshare.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
