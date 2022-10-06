c9eb0566efde8460d4d6c43bfacc7e388faa9220
=Albert Montero <=montero.villar.albert@alumnat.copernic.cat>
2022-10-06 12:16:40 +0200

    Commit de Testeo

--- app/src/main/java/cat/copernic/fpshare/Registro.kt
+++ app/src/main/java/cat/copernic/fpshare/Registro.kt
@@ -2,12 +2,11 @@ package cat.copernic.fpshare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import cat.copernic.fpshare.R
 
-class MainActivity : AppCompatActivity() {
+class registro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_registro)
     }
 }
