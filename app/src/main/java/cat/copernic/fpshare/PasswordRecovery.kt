fefee7cea8911fff2da0a63e83026b083c8fb591
=Andres Rojas <=rojas.alzate.andres@alumnat.copernic.cat>
2022-10-10 13:26:17 +0200

    commit de pantallas login, recuperar contraseña y perfil

--- app/src/main/java/cat/copernic/fpshare/PasswordRecovery.kt
+++ app/src/main/java/cat/copernic/fpshare/PasswordRecovery.kt
@@ -2,13 +2,10 @@ package cat.copernic.fpshare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import cat.copernic.fpshare.R
 
-class MainActivity : AppCompatActivity() {
+class PasswordRecovery : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        //Hacemos algun cambio
+        setContentView(R.layout.activity_password_recovery)
     }
 }
