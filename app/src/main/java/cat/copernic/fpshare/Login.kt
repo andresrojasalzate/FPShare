e4caf0706482711553297a66489e79f60e94653f
trujillo.agut.carles <trujillo.agut.carles@alumnat.copernic.cat>
2022-10-26 08:25:26 +0200

    Filtro de errores en login

--- app/src/main/java/cat/copernic/fpshare/Login.kt
+++ app/src/main/java/cat/copernic/fpshare/Login.kt
@@ -13,70 +13,74 @@ import com.google.firebase.ktx.Firebase
 
 class Login : AppCompatActivity() {
 
-    private lateinit var correoLogin:EditText
-    private lateinit var passwordLogin:EditText
+    private lateinit var correoLogin: EditText
+    private lateinit var passwordLogin: EditText
     private lateinit var bontonLogin: Button
-    private lateinit var bontonRegistre:Button
+    private lateinit var bontonRegistre: Button
     private lateinit var textViewForgotPassword: TextView
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Thread.sleep(2000)
-
-         setTheme(R.style.Theme_Fpshare)
+        setTheme(R.style.Theme_Fpshare)
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
-       inicializacion()
+        inicializacion()
 
-       bontonLogin.setOnClickListener {
-           val correo = correoLogin.text.toString()
-           val password = passwordLogin.text.toString()
+        bontonLogin.setOnClickListener {
+            val correo = correoLogin.text.toString()
+            val password = passwordLogin.text.toString()
 
-           if(camposVacios(correo, password)){
-               login(correo, password)
-           } else{
-               Toast.makeText(applicationContext,"Faltan campos por completar", Toast.LENGTH_LONG).show()
-           }
-       }
-       bontonRegistre.setOnClickListener {
+            if (camposVacios(correo, password)) {
+                login(correo, password)
+            } else {
+                error()
+            }
+        }
+        bontonRegistre.setOnClickListener {
 
-           startActivity(Intent(this,Register::class.java))
-           finish()
-       }
-        textViewForgotPassword.setOnClickListener {
-            startActivity(Intent(this,RecoveryPassword::class.java))
+            startActivity(Intent(this, Register::class.java))
             finish()
         }
-   }
-  private fun inicializacion(){
+        textViewForgotPassword.setOnClickListener {
+            startActivity(Intent(this, RecoveryPassword::class.java))
+            finish()
+        }
+    }
 
-      correoLogin = findViewById(R.id.edittext_email_login)
-      passwordLogin = findViewById(R.id.edittext_password_login)
-      bontonLogin = findViewById(R.id.button_singin_login)
-      bontonRegistre = findViewById(R.id.button_register_login)
-      textViewForgotPassword = findViewById(R.id.textView_forgotten_password)
-      auth= Firebase.auth
+    private fun inicializacion() {
 
-   }
+        correoLogin = findViewById(R.id.edittext_email_login)
+        passwordLogin = findViewById(R.id.edittext_password_login)
+        bontonLogin = findViewById(R.id.button_singin_login)
+        bontonRegistre = findViewById(R.id.button_register_login)
+        textViewForgotPassword = findViewById(R.id.textView_forgotten_password)
+        auth = Firebase.auth
 
-   private fun camposVacios(correo:String, password:String): Boolean{
-       return correo.isNotEmpty()&&password.isNotEmpty()
-   }
+    }
 
-   private fun login(correo:String, password:String){
+    private fun camposVacios(correo: String, password: String): Boolean {
+        return correo.isNotEmpty() && password.isNotEmpty()
+    }
 
-       auth.signInWithEmailAndPassword(correo,password)
-           .addOnCompleteListener(this) {task ->
-               if(task.isSuccessful){
-                   startActivity(Intent(this,MainActivity::class.java))
-                   finish()
-               }else{
-                   Toast.makeText(applicationContext,"El login ha fallado", Toast.LENGTH_LONG).show()
-               }
-           }
+    private fun login(correo: String, password: String) {
+
+        auth.signInWithEmailAndPassword(correo, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                } else {
+                    error()
+                }
+            }
 
 
+    }
+
+    private fun error() {
+        Toast.makeText(applicationContext, "Wrong user or password", Toast.LENGTH_LONG).show()
     }
 }
