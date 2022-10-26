e96e253a9d822fefde66bc8d562edb53c4896237
=Albert Montero <=montero.villar.albert@alumnat.copernic.cat>
2022-10-26 10:36:29 +0200

    commit 26/10/2022 Actualizacion de cambios

--- app/src/main/java/cat/copernic/fpshare/CicleAdapter.kt
+++ app/src/main/java/cat/copernic/fpshare/CicleAdapter.kt
@@ -1,28 +1,74 @@
 package cat.copernic.fpshare
 
+import android.content.Intent
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.accessibility.AccessibilityNodeInfo
+import android.widget.Button
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
+import java.util.Collections.addAll
+import java.util.stream.Collectors.toList
 
-class CicleAdapter(var cicle:List<String>): RecyclerView.Adapter<CicleAdapter.CicleHolder>() {
+class CicleAdapter :
+    RecyclerView.Adapter<CicleAdapter.CicleViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CicleHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        return CicleHolder(layoutInflater.inflate(R.layout.fragment_menu_ciclos, parent, false))
+    private val list: List<String> = listOf("SMIX", "DAM", "DAW", "ASIX")
+
+    class CicleViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val button = view.findViewById<Button>(R.id.button_list)
     }
 
-    override fun getItemCount(): Int = cicle.size
-
-
-    override fun onBindViewHolder(holder: CicleAdapter.CicleHolder, position: Int) {
-        holder.render(cicle[position])
+    override fun getItemCount(): Int {
+        return list.size
     }
 
-    class CicleHolder(val view:View):RecyclerView.ViewHolder(view){
-        fun render(cicle: String){
-           view.txtCiclo.text = cicle.toString()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CicleViewHolder {
+        val layout = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_view, parent, false)
+        layout.accessibilityDelegate = Accessibility
+        return CicleViewHolder(layout)
+    }
+
+    override fun onBindViewHolder(holder: CicleViewHolder, position: Int) {
+        val item = list.get(position)
+        holder.button.text = item.toString()
+        holder.button.setOnClickListener {
+            val context = holder.itemView.context
+            val intent = Intent(context, MenuCiclos::class.java)
+            intent.putExtra("cicle", holder.button.text.toString())
+            context.startActivity(intent)
+        }
+    }
+
+    companion object Accessibility : View.AccessibilityDelegate() {
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        override fun onInitializeAccessibilityNodeInfo(
+            host: View,
+            info: AccessibilityNodeInfo
+        ) {
+            super.onInitializeAccessibilityNodeInfo(host, info)
+            // With `null` as the second argument to [AccessibilityAction], the
+            // accessibility service announces "double tap to activate".
+            // If a custom string is provided,
+            // it announces "double tap to <custom string>".
+            val customString = host.context?.getString(R.string.look_up_words)
+            val customClick =
+                AccessibilityNodeInfo.AccessibilityAction(
+                    AccessibilityNodeInfo.ACTION_CLICK,
+                    customString
+                )
+            info.addAction(customClick)
         }
     }
 
 }
+
+
+
+
+
--- app/src/main/java/cat/copernic/fpshare/Register.kt
+++ app/src/main/java/cat/copernic/fpshare/Register.kt
@@ -1,12 +1,71 @@
 package cat.copernic.fpshare
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import cat.copernic.fpshare.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Register : AppCompatActivity() {
+
+    private lateinit var btnRegistrarse: Button
+    private lateinit var InputNombre: EditText
+    private lateinit var InputPassword: EditText
+    private lateinit var InputMail: EditText
+    private lateinit var auth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+
+        //Inicializacion de componentes
+        InputNombre = findViewById(R.id.InputNombre)
+        InputPassword = findViewById(R.id.InputPassword)
+        InputMail = findViewById(R.id.InputMail)
+        btnRegistrarse = findViewById(R.id.btnRegistrarse)
+
+
+        //Inicializacion Firebase
+        auth = Firebase.auth
+
+        btnRegistrarse.setOnClickListener(){
+            var nombre = InputNombre.text.toString()
+            var password = InputPassword.text.toString()
+            var mail = InputMail.text.toString()
+
+            if(campEsBuit(nombre,password,mail)){
+                //Registrem a l'usuari mitjançant la funció registrar creada per nosaltres
+                registrar(nombre, password,mail)
+            }
+
+        }
+
+
     }
+
+    fun campEsBuit(nombre:String,password:String, mail:String):Boolean{
+        return nombre.isNotEmpty()&&password.isNotEmpty()&&mail.isNotEmpty()
+    }
+
+    fun registrar(nombre: String,password: String,mail: String){
+
+        auth.createUserWithEmailAndPassword(mail,password)
+            .addOnCompleteListener(this) {task ->
+                if(task.isSuccessful){ //El registre (task) s'ha completat amb exit...
+                    //Anem al mainActivity des d'aquesta pantalla
+                    startActivity(Intent(this,MainActivity::class.java))
+                    finish() //Alliberem memòria un cop finalitzada aquesta tasca.
+                }else{
+                    Toast.makeText(applicationContext,"El registre ha fallat!", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+
 }
