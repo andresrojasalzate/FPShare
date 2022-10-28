2561972c065698d441a27a0ca0a8696d05adc273
=Albert Montero <=montero.villar.albert@alumnat.copernic.cat>
2022-10-28 09:21:42 +0200

    Arreglado Registro

--- app/src/main/java/cat/copernic/fpshare/CicleAdapter.kt
+++ app/src/main/java/cat/copernic/fpshare/CicleAdapter.kt
@@ -1,6 +1,5 @@
 package cat.copernic.fpshare
 
-import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
@@ -9,9 +8,9 @@ import android.view.accessibility.AccessibilityNodeInfo
 import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
 
-class CicleAdapter(private val ciclo: String, context: Context) :
+class CicleAdapter(private val cicleArrayList: ArrayList<String>/*, context: Context*/) :
     RecyclerView.Adapter<CicleAdapter.CicleViewHolder>() {
-
+    /*
     private val cicloElegido: List<String>
 
     init {
@@ -20,13 +19,14 @@ class CicleAdapter(private val ciclo: String, context: Context) :
             .filter { it.equals(ciclo, ignoreCase = true) }
             .shuffled()
     }
+    */
 
     override fun getItemCount(): Int {
-        return cicloElegido.size
+        return cicleArrayList.size
     }
 
     class CicleViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        val button = view.findViewById<Button>(R.id.button_list)
+        val button = view.findViewById<Button>(R.id.button_list)!!
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CicleViewHolder {
@@ -38,8 +38,15 @@ class CicleAdapter(private val ciclo: String, context: Context) :
     }
 
     override fun onBindViewHolder(holder: CicleViewHolder, position: Int) {
-        val item = cicloElegido[position]
+        val currentItem = cicleArrayList[position]
+        holder.button.text = currentItem
+        holder.button.setOnClickListener{
+            val context = holder.itemView.context
+            val intent = Intent(context, MenuModulo::class.java)
+            intent.putExtra("cicle", holder.button.text.toString())
+        }
 
+        /*val item = cicloElegido[position]
 
         holder.button.text = item
 
@@ -48,7 +55,7 @@ class CicleAdapter(private val ciclo: String, context: Context) :
             val intent = Intent(context, MenuCiclos::class.java)
             intent.putExtra("cicle", holder.button.text.toString())
             context.startActivity(intent)
-        }
+        }*/
     }
 
     companion object Accessibility : View.AccessibilityDelegate() {
@@ -70,6 +77,5 @@ class CicleAdapter(private val ciclo: String, context: Context) :
             info.addAction(customClick)
         }
     }
-
 }
 
