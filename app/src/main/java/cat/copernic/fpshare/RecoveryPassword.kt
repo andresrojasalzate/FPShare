deb1bff703034edd2ac5bb7c2683cdc951f7085e
=Andres Rojas <=rojas.alzate.andres@alumnat.copernic.cat>
2022-10-28 09:51:36 +0200

    navegación de la pantalla de recovery password

--- app/src/main/java/cat/copernic/fpshare/RecoveryPassword.kt
+++ app/src/main/java/cat/copernic/fpshare/RecoveryPassword.kt
@@ -1,11 +1,22 @@
 package cat.copernic.fpshare
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RecoveryPassword : AppCompatActivity() {
+    private lateinit var bontonRecoveryPassword: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recovery_password)
+
+        bontonRecoveryPassword = findViewById(R.id.button_singin_recoverypassword)
+
+        bontonRecoveryPassword.setOnClickListener {
+            startActivity(Intent(this, Login::class.java))
+        }
+
     }
 }
