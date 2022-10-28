358e2504ad82dfa4d7608101615fdbb5f33f263f
trujillo.agut.carles <trujillo.agut.carles@alumnat.copernic.cat>
2022-10-28 09:02:37 +0200

    Correccion de errores // Añadido nuevo adapter // Corregido Splash screen

---
+++ app/src/main/java/cat/copernic/fpshare/PubliAdapter.kt
@@ -0,0 +1,22 @@
+package cat.copernic.fpshare
+
+/*class PubliAdapter(private val publicacion: String, context: Context) :
+    RecyclerView.Adapter<PubliAdapter.PubliViewHolder> {
+
+    private val publicacionList: List<String>
+
+    init {
+        val ciclos = context.resources.getStringArray(R.array.publicacion).toList()
+        publicacionList = ciclos
+            .filter { it.equals(publicacion, ignoreCase = true) }
+            .shuffled()
+    }
+
+    override fun getItemCount(): Int {
+        return publicacionList.size
+    }
+
+        class PubliViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+
+        }
+} */
