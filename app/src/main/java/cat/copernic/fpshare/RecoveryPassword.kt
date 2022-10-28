6666c87ca876be23406a43be9110b2e11eada9b5
trujillo.agut.carles <trujillo.agut.carles@alumnat.copernic.cat>
2022-10-28 10:05:07 +0200

    Creado intent para recovery password

--- app/src/main/java/cat/copernic/fpshare/RecoveryPassword.kt
+++ app/src/main/java/cat/copernic/fpshare/RecoveryPassword.kt
@@ -1,11 +1,34 @@
 package cat.copernic.fpshare
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class RecoveryPassword : AppCompatActivity() {
+
+    private lateinit var emailRecovery: EditText
+    private lateinit var buttonRecovery: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recovery_password)
+
+        init()
+
+        listeners()
+    }
+
+    private fun init() {
+        emailRecovery = findViewById(R.id.editText_recovery)
+        buttonRecovery = findViewById(R.id.button_signin_recoverypassword)
+    }
+
+    private fun listeners() {
+        buttonRecovery.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 }
