fc6ae724c39c660a9eb57d095efc3a44c2acbe55
=Andres Rojas <=rojas.alzate.andres@alumnat.copernic.cat>
2022-11-01 13:26:26 +0100

    commit

--- app/src/main/java/cat/copernic/fpshare/RecoveryPassword.kt
+++ app/src/main/java/cat/copernic/fpshare/RecoveryPassword.kt
@@ -6,17 +6,9 @@ import android.os.Bundle
 import android.widget.Button
 
 class RecoveryPassword : AppCompatActivity() {
-    private lateinit var bontonRecoveryPassword: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recovery_password)
-
-        bontonRecoveryPassword = findViewById(R.id.button_singin_recoverypassword)
-
-        bontonRecoveryPassword.setOnClickListener {
-            startActivity(Intent(this, Login::class.java))
-        }
-
     }
 }
