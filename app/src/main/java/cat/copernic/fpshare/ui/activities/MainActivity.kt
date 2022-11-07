b19a2b39f25080b6546c1aeed5c942177df59bbe
montero.villar.albert <montero.villar.albert@alumnat.copernic.cat>
2022-11-07 20:02:48 +0100

    Menu Navigation Bottom Terminado

--- app/src/main/java/cat/copernic/fpshare/ui/activities/MainActivity.kt
+++ app/src/main/java/cat/copernic/fpshare/ui/activities/MainActivity.kt
@@ -1,11 +1,16 @@
 package cat.copernic.fpshare.ui.activities
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import cat.copernic.fpshare.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
@@ -19,12 +24,26 @@ class MainActivity : AppCompatActivity() {
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         // Instantiate the navController using the NavHostFragment
         navController = navHostFragment.navController
+        setupBottomNavMenu(navController)
         // Make sure actions in the ActionBar get propagated to the NavController
         setupActionBarWithNavController(navController)
+        setupBottomNavMenu(navController);
 
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
+
+    private fun setupBottomNavMenu(navController: NavController){
+        val bottomNav=findViewById<BottomNavigationView>(R.id.nav_view)
+        bottomNav?.setupWithNavController(navController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(
+            findNavController(R.id.nav_host_fragment)
+        ) || super.onOptionsItemSelected(item)
+    }
+
 }
