6470de185d3761413d81ff19d3834ffa9a3af2d4
trujillo.agut.carles <trujillo.agut.carles@alumnat.copernic.cat>
2022-11-10 13:20:28 +0100

    Boton guardar en ajustes ahora te manda a la pantalla principal

--- app/src/main/java/cat/copernic/fpshare/ui/fragments/FPAjustes.kt
+++ app/src/main/java/cat/copernic/fpshare/ui/fragments/FPAjustes.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import cat.copernic.fpshare.databinding.FragmentFpAjustesBinding
 
 
 class FPAjustes : Fragment() {
     private var _binding: FragmentFpAjustesBinding? = null
     private val binding get() = _binding!!
+    private lateinit var buttonSave: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,14 +24,19 @@ class FPAjustes : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentFpAjustesBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
+        buttonSave = binding.btnSubmit
+        buttonSave.setOnClickListener {
+            val action =
+                FPAjustesDirections.actionFpAjustesToPantallaPrincipal()
+            view.findNavController().navigate(action)
+        }
     }
 
     override fun onDestroyView() {
