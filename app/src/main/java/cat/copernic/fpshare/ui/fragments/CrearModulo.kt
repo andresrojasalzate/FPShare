6dd698e4b26f9118a6260ce552bd25f553230df6
trujillo.agut.carles <trujillo.agut.carles@alumnat.copernic.cat>
2022-11-17 10:48:07 +0100

    Corregido error con ID de los fragments añadidos en el anterior commit // Añadida navegacion

--- app/src/main/java/cat/copernic/fpshare/ui/fragments/CrearModulo.kt
+++ app/src/main/java/cat/copernic/fpshare/ui/fragments/CrearModulo.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [CrearTag.newInstance] factory method to
+ * Use the [CrearModulo.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CrearTag : Fragment() {
+class CrearModulo : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class CrearTag : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_crear_tag, container, false)
+        return inflater.inflate(R.layout.fragment_crear_modulo, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class CrearTag : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment CrearTag.
+         * @return A new instance of fragment CrearModulo.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            CrearTag().apply {
+            CrearModulo().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
