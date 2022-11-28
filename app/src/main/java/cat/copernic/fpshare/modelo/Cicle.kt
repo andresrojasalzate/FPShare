e02b5b2bd0b1d59f98e22c69e2abf5041ebc226a
CarlesTrujillo <agut.trujillo65@gmail.com>
2022-11-28 13:16:12 +0100

    Eliminadas las listas de clases Ciclo, Modulo y UF

--- app/src/main/java/cat/copernic/fpshare/modelo/Cicle.kt
+++ app/src/main/java/cat/copernic/fpshare/modelo/Cicle.kt
@@ -2,5 +2,5 @@ package cat.copernic.fpshare.modelo
 
 import com.google.firebase.firestore.Exclude
 
-data class Cicle(@get:Exclude var idCiclo: String, var nombre: String, var Modulos: List<Modul>?)
+data class Cicle(@get:Exclude var idCiclo: String, var nombre: String)
 
--- app/src/main/java/cat/copernic/fpshare/modelo/Uf.kt
+++ app/src/main/java/cat/copernic/fpshare/modelo/Uf.kt
@@ -2,4 +2,4 @@ package cat.copernic.fpshare.modelo
 
 import com.google.firebase.firestore.Exclude
 
-data class Uf(@get:Exclude var idUf: String, var nombre: String, var publicaciones: List<Publicacion>)
+data class Uf(@get:Exclude var idUf: String, var nombre: String)
