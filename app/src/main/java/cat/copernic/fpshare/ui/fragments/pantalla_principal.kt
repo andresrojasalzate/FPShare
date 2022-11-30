8c68bda0a79dbdf64c2d3d6a3332668b268c81e4
=Albert Montero <=montero.villar.albert@alumnat.copernic.cat>
2022-11-30 09:12:00 +0100

    Commmit 30/11/2022 Borrado boton log out i correcciones menores.

--- app/src/main/java/cat/copernic/fpshare/ui/fragments/pantalla_principal.kt
+++ app/src/main/java/cat/copernic/fpshare/ui/fragments/pantalla_principal.kt
@@ -2,7 +2,6 @@ package cat.copernic.fpshare.ui.fragments
 
 import android.os.Bundle
 import android.view.*
-import android.widget.Adapter
 import androidx.fragment.app.Fragment
 import android.widget.Button
 import androidx.lifecycle.lifecycleScope
@@ -10,13 +9,9 @@ import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import cat.copernic.fpshare.adapters.PubliAdapter
-import cat.copernic.fpshare.adapters.UfAdminAdapter
 import cat.copernic.fpshare.databinding.FragmentPantallaPrincipalBinding
 import cat.copernic.fpshare.modelo.Publicacion
-import cat.copernic.fpshare.modelo.Uf
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -25,7 +20,6 @@ class pantalla_principal : Fragment() {
     private var _binding: FragmentPantallaPrincipalBinding? = null
     private val binding get() = _binding!!
     private lateinit var boton: Button
-    private lateinit var btn_logout: Button
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: PubliAdapter
     private lateinit var cicloList: MutableList<Publicacion>
@@ -49,7 +43,6 @@ class pantalla_principal : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         boton = binding.button3
-        btn_logout = binding.btnLogout
         recyclerView = binding.recyclerView
 
         lifecycleScope.launch(Dispatchers.Main){
@@ -61,15 +54,6 @@ class pantalla_principal : Fragment() {
                 pantalla_principalDirections.actionPantallaPrincipalToVistaPreviaPublicacion()
             view.findNavController().navigate(action)
         }
-        btn_logout.setOnClickListener{
-            Firebase.auth.signOut()
-            val action =
-                pantalla_principalDirections.actionPantallaPrincipalToLogin()
-            view.findNavController().navigate(action)
-
-
-        }
-
     }
 
     override fun onDestroyView() {
@@ -80,8 +64,6 @@ class pantalla_principal : Fragment() {
     private fun crearMenu(): MutableList<Publicacion>{
         var cicloList = mutableListOf<Publicacion>()
         val ciclo = bd.collection("Ciclos")
-        //val modulo = ciclo
-        //bd.collection("Ciclos").document()
         ciclo.get().addOnSuccessListener { docuciclos ->
             for (docuciclo in docuciclos){
                 val modulo =ciclo.document(docuciclo.id).collection("Modulos")
@@ -119,28 +101,6 @@ class pantalla_principal : Fragment() {
             }
 
         }
-                /*
-            .collection("Modulos").document()
-            .collection("UFs").document()
-            .collection("Publicaciones")
-            .get()
-            .addOnSuccessListener { documents ->
-                for (document in documents){
-                    val idPubli = document.id
-                    val checked = document["checked"].toString()
-                    val publiDescr = document["descripcion"].toString()
-                    val publiLink = document["enlace"].toString()
-                    val publiProfile = document["perfil"].toString()
-                    val publiTitle = document["titulo"].toString()
-                    val publi = Publicacion(idPubli,publiProfile,publiTitle,publiDescr,checked,publiLink)
-                    cicloList.add(publi)
-                }
-                adapter= PubliAdapter(cicloList)
-                binding.recyclerView.adapter = adapter
-                binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
-            }
-    */
-
         return cicloList
     }
 
