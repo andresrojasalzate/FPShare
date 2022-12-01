960aeee591bcb1962a0d11a08f74edb10accf4fd
=Albert Montero <=montero.villar.albert@alumnat.copernic.cat>
2022-12-01 09:47:59 +0100

    Commmit 30/11/2022 Arreglado el item_menu. Ahora muestra bien la url i muestra aparte, el titulo de la publicacion, que no estava puesto antes.

--- app/src/main/java/cat/copernic/fpshare/ui/fragments/pantalla_principal.kt
+++ app/src/main/java/cat/copernic/fpshare/ui/fragments/pantalla_principal.kt
@@ -2,7 +2,6 @@ package cat.copernic.fpshare.ui.fragments
 
 import android.os.Bundle
 import android.view.*
-import android.widget.Adapter
 import androidx.fragment.app.Fragment
 import android.widget.Button
 import androidx.fragment.app.Fragment
@@ -10,13 +9,9 @@ import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import cat.copernic.fpshare.adapters.PubliAdapter
-import cat.copernic.fpshare.adapters.UfAdminAdapter
 import cat.copernic.fpshare.databinding.FragmentPantallaPrincipalBinding
 import cat.copernic.fpshare.modelo.Publicacion
-import cat.copernic.fpshare.modelo.Uf
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -62,8 +57,6 @@ class pantalla_principal : Fragment() {
     private fun crearMenu(): MutableList<Publicacion>{
         var cicloList = mutableListOf<Publicacion>()
         val ciclo = bd.collection("Ciclos")
-        //val modulo = ciclo
-        //bd.collection("Ciclos").document()
         ciclo.get().addOnSuccessListener { docuciclos ->
             for (docuciclo in docuciclos){
                 val modulo =ciclo.document(docuciclo.id).collection("Modulos")
@@ -102,28 +95,6 @@ class pantalla_principal : Fragment() {
             }
 
         }
-                /*
-            .collection("Modulos").document()
-            .collection("UFs").document()
-            .collection("Publicaciones")
-            .get()
-            .addOnSuccessListener { documents ->
-                for (document in documents){
-                    val idPubli = document.id
-                    val checked = document["checked"].toString()
-                    val publiDescr = document["descripcion"].toString()
-                    val publiLink = document["enlace"].toString()
-                    val publiProfile = document["perfil"].toString()
-                    val publiTitle = document["titulo"].toString()
-                    val publi = Publicacion(idPubli,publiProfile,publiTitle,publiDescr,checked,publiLink)
-                    cicloList.add(publi)
-                }
-                adapter= PubliAdapter(cicloList)
-                binding.recyclerView.adapter = adapter
-                binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
-            }
-    */
-
         return cicloList
     }
 
