32becf7d9eaf27a72d2b98fe963063437282628a
=Albert Montero <=montero.villar.albert@alumnat.copernic.cat>
2022-12-05 10:10:25 +0100

    Commmit 05/12/2022 Publicaciones principales implementado suspend function i funciones await/async

--- app/src/main/java/cat/copernic/fpshare/ui/fragments/pantalla_principal.kt
+++ app/src/main/java/cat/copernic/fpshare/ui/fragments/pantalla_principal.kt
@@ -2,8 +2,7 @@ package cat.copernic.fpshare.ui.fragments
 
 import android.os.Bundle
 import android.view.*
-import androidx.fragment.app.Fragment
-import android.widget.Button
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,16 +11,15 @@ import cat.copernic.fpshare.adapters.PubliAdapter
 import cat.copernic.fpshare.databinding.FragmentPantallaPrincipalBinding
 import cat.copernic.fpshare.modelo.Publicacion
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
+import kotlinx.coroutines.tasks.await
 
 class pantalla_principal : Fragment() {
     private var _binding: FragmentPantallaPrincipalBinding? = null
     private val binding get() = _binding!!
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: PubliAdapter
-    private lateinit var cicloList: MutableList<Publicacion>
+    private lateinit var cicloList: Deferred<MutableList<Publicacion>>
     val bd = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,7 +42,7 @@ class pantalla_principal : Fragment() {
         recyclerView = binding.recyclerView
 
         lifecycleScope.launch(Dispatchers.Main){
-            cicloList = withContext(Dispatchers.IO){ crearMenu()}
+           cicloList = async{ crearMenu()}
         }
 
     }
@@ -54,47 +52,40 @@ class pantalla_principal : Fragment() {
         _binding = null
     }
 
-    private fun crearMenu(): MutableList<Publicacion>{
+    suspend fun crearMenu(): MutableList<Publicacion>{
         var cicloList = mutableListOf<Publicacion>()
-        val ciclo = bd.collection("Ciclos")
-        ciclo.get().addOnSuccessListener { docuciclos ->
-            for (docuciclo in docuciclos){
-                val modulo =ciclo.document(docuciclo.id).collection("Modulos")
-                modulo.get().addOnSuccessListener { documodulos ->
-                    for (documodulo in documodulos){
-                        val uf_s =modulo.document(documodulo.id).collection("UFs")
-                            uf_s.get().addOnSuccessListener { docuufs ->
-                                for (docuuf in docuufs){
-                                     uf_s.document(docuuf.id).collection("Publicaciones")
-                                        .get()
-                                        .addOnSuccessListener { docupublis ->
-                                            for (docupubli in docupublis){
-                                                    val idPubli = docupubli.id
-                                                    val checked = docupubli["checked"].toString()
-                                                    val publiDescr = docupubli["descripcion"].toString()
-                                                    val publiLink = docupubli["enlace"].toString()
-                                                    val publiProfile = docupubli["perfil"].toString()
-                                                    val publiTitle = docupubli["titulo"].toString()
-                                                    val publi = Publicacion(idPubli,publiProfile,publiTitle,publiDescr,checked,publiLink)
-                                                    cicloList.add(publi)
-                                            }
-                                            adapter = PubliAdapter(cicloList)
-                                            binding.recyclerView.adapter = adapter
-                                            binding.recyclerView.layoutManager =
-                                                LinearLayoutManager(requireContext())
-                                        }
-
+        val ciclo = bd.collection("Ciclos").get().await()
+            for(doc1 in ciclo){
+                val modulo = bd.collection("Ciclos").document(doc1.id).collection("Modulos").get().await()
+                    for(doc2 in modulo){
+                        val uf = bd.collection("Ciclos").document(doc1.id).collection("Modulos").document(doc2.id).collection("UFs").get().await()
+                        for(doc3 in uf){
+                            val publi = bd.collection("Ciclos").document(doc1.id).collection("Modulos").document(doc2.id).collection("UFs").document(doc3.id).collection("Publicaciones").get().await()
+                                for(doc4 in publi){
+                                        val idPubli = doc4.id
+                                        val checked = doc4["checked"].toString()
+                                        val publiDescr = doc4["descripcion"].toString()
+                                        var publiLink = doc4["enlace"].toString()
+                                        val publiProfile = doc4["perfil"].toString()
+                                        val publiTitle = doc4["titulo"].toString()
+                                        val publi = Publicacion(
+                                            idPubli,
+                                            publiProfile,
+                                            publiTitle,
+                                            publiDescr,
+                                            checked,
+                                            publiLink
+                                        )
+                                        adapter = PubliAdapter(cicloList)
+                                        binding.recyclerView.adapter = adapter
+                                        binding.recyclerView.layoutManager =
+                                            LinearLayoutManager(requireContext())
+                                        cicloList.add(publi)
 
                                 }
-
-                            }
+                        }
                     }
-
-                }
-
             }
-
-        }
         return cicloList
     }
 
