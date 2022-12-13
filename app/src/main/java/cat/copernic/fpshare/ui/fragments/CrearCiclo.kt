8331c61c641dfb351b7635930e73c71227cb0f59
trujillo.agut.carles <trujillo.agut.carles@alumnat.copernic.cat>
2022-12-13 12:55:03 +0100

    Modificado crearciclo, item para ver ciclos, y navGraph con navegación de crearCiclo hacia atrás // Nueva animación añadida en Foros-Hilo

--- app/src/main/java/cat/copernic/fpshare/ui/fragments/CrearCiclo.kt
+++ app/src/main/java/cat/copernic/fpshare/ui/fragments/CrearCiclo.kt
@@ -6,11 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import cat.copernic.fpshare.databinding.FragmentCrearCicloBinding
 import cat.copernic.fpshare.modelo.Cicle
 import com.google.firebase.firestore.FirebaseFirestore
-
 
 class CrearCiclo : Fragment() {
     private var _binding: FragmentCrearCicloBinding? = null
@@ -58,14 +59,32 @@ class CrearCiclo : Fragment() {
                 val ciclo = Cicle(id, nombre)
                 addCiclo(ciclo, id)
             }
+            ciclosBack()
         }
     }
 
+    // Adición del ciclo nuevo en la base de datos
     private fun addCiclo(ciclo: Cicle, id: String) {
+        val appContext = context
         bd.collection("Ciclos").document(id).set(ciclo)
+            .addOnSuccessListener {
+                Toast.makeText(appContext, "Ciclo añadido correctamente", Toast.LENGTH_LONG).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(appContext, "Error al añadir el ciclo", Toast.LENGTH_LONG).show()
+            }
     }
 
+    // Comprobar que no tenga campos vacios o en blanco
     private fun campoVacio(ID: String, nombre: String): Boolean {
         return ID.isNotEmpty() && nombre.isNotEmpty() && ID.isNotBlank() && nombre.isNotBlank()
+    }
+
+    // Función para volver hacia atrás después de crear el nuevo ciclo
+    private fun ciclosBack() {
+        val view = binding.root
+        val action =
+            CrearCicloDirections.actionCrearCicloToListaTagsAdministracion()
+        view.findNavController().navigate(action)
     }
 }
