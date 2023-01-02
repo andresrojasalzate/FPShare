6bd52763d77c935864680f55c1fde94215a2ea8f
montero.villar.albert <montero.villar.albert@alumnat.copernic.cat>
2023-01-02 18:03:31 +0100

    Añadido funcion enlace en el Adapter de Publicaciones.

--- app/src/main/java/cat/copernic/fpshare/adapters/PubliAdapter.kt
+++ app/src/main/java/cat/copernic/fpshare/adapters/PubliAdapter.kt
@@ -1,7 +1,9 @@
 package cat.copernic.fpshare.adapters
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView
 import cat.copernic.fpshare.R
 import cat.copernic.fpshare.databinding.ItemPubliBinding
 import cat.copernic.fpshare.modelo.Publicacion
+import cat.copernic.fpshare.ui.fragments.ENLACE
 import com.google.firebase.storage.FirebaseStorage
 import java.io.File
 
@@ -34,16 +37,35 @@ class PubliAdapter(private val publicaciones: List<Publicacion>) : RecyclerView.
         val publicacion = publicaciones.get(position)
 
         with(viewHolder) {
+            /***
+             * Ponemos los textos en cada recuadro del Item Publi.
+             */
             viewB.txtProf.text = publicacion.perfil
             viewB.txtPubliTitle.text = publicacion.titulo
             viewB.txtDescr.text = publicacion.descripcion
             viewB.textLink.text = publicacion.enlace
+
+            /***
+             * Carga de la ruta del enlace a la imagen de la publicacion
+             */
             val storageRef = storage.reference.child("Imagenes/" + publicacion.imgPubli)
 
+            /***
+             * Colocamos la imagen en el ImageView del Item Publi.
+             */
             val localfile = File.createTempFile("tempImage", "jpg")
             storageRef.getFile(localfile).addOnSuccessListener {
                 val bitmap = BitmapFactory.decodeFile(localfile.absolutePath)
                 viewB.imgIcon.setImageBitmap(bitmap)
+            }
+            /***
+             * Inicializacion del enlace
+             */
+            viewB.textLink.setOnClickListener{
+                val queryUrl: Uri = Uri.parse(publicacion.enlace)
+                val intent = Intent(Intent.ACTION_VIEW, queryUrl)
+                contexto?.startActivity(intent)
+
             }
         }
 
